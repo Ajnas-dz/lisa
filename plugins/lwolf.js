632e46851a1cad52ad888d6197d3ca2931a4543d
CHINJUMWOL <90092049+Ajnas-dz@users.noreply.github.com>
2021-09-15 11:15:39 +0530

    Update lwolf.js

--- plugins/lwolf.js
+++ plugins/lwolf.js
@@ -8,7 +8,7 @@ const { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys'
 const fs = require('fs');
 const axios = require('axios');
 const Config = require('../config');
-const need = "*type some word after command*\n*command  ENTHEKILUM EZHUTH NENBA"
+const need = "*type some word after command*\n*command  ENTHEKILUM EZUTH DA"
 
 if (Config.WORKTYPE == 'private') {
 
@@ -18,7 +18,7 @@ if (Config.WORKTYPE == 'private') {
 
         var ttinullimage = await axios.get(`https://api.zeks.xyz/api/wolflogo?apikey=cjxJIn9eDletRHac3CG6CdvGxFH&text1=LiyaMol&text2=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
 
-        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: 'Made by Pikachu' })
+        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: 'Made by ℂ𝕙𝕚𝕟𝕛𝕦𝕄𝕨𝕠𝕝 })
 
     }));
 }
@@ -29,7 +29,7 @@ else if (Config.WORKTYPE == 'public') {
 
         if (match[1] === '') return await message.sendMessage(need);
 
-        var ttinullimage = await axios.get(`https://api.zeks.xyz/api/wolflogo?apikey=cjxJIn9eDletRHac3CG6CdvGxFH&text1=LiyaMol&text2=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
+        var ttinullimage = await axios.get(`https://api.zeks.xyz/api/wolflogo?apikey=cjxJIn9eDletRHac3CG6CdvGxFH&text1=ChinjuMwol&text2=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
 
         await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: 'Made by Pikachu' })
 
